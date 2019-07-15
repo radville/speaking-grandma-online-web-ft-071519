0ab29c8be07cbd03f38e361a7f3d7cbb081080bc
Laura Radville <laura.radville@gmail.com>
2019-07-15 13:59:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 def speak_to_grandma(string)
-
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-      if string == string.upcase
+  elsif string == string.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
